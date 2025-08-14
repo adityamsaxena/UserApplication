700d9c7239f929e00ced9e2aab35730630743fd1
aditya saxena <saxenamaditya@gmail.com>
2025-08-14 12:09:36 +0530

    update file import in sideBar

--- src/layouts/sidebar.jsx
+++ src/layouts/sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./sidebar.css";
+import "./sideBar.css";
 import Button from "../components/button";
 
 const Sidebar = ({ isSideBarOpen, setIsSideBarOpen }) => {
